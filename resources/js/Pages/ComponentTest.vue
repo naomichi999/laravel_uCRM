3ccb3b6eac535316e152956f4d549284b535da9b
naomichi999 <naomichi999@gmail.com>
2024-03-03 18:29:40 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,20 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+
+</script>
+
+<template>
+  <GuestLayout>
+    <Label value="件名">タイトル</Label>
+    <!-- @update:modelValueはemit処理のこと。子コンポーネントで入力があればemitTestの処理が走る -->
+    <Input
+      modelValue="初期値が入ります"
+      @update:modelValue="emitTest"
+    ></Input>
+  </GuestLayout>
+  
+</template>
